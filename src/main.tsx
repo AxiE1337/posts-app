80542ccfb8083d0f3acc96bb526443ed2ae31107
AxiE1337 <pixell390@gmail.com>
2023-05-22 15:47:55 +0300

    added layout route

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,32 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Posts from './pages/PostsPage/Posts.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import { store } from './store/index.ts'
 import { Provider } from 'react-redux'
 import Header from './components/Header.tsx'
 import AboutMe from './pages/AboutMePage/AboutMe.tsx'
 import User from './pages/UserPage/User.tsx'
 
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Header />
         <Routes>
-          <Route path='/' element={<Posts />} />
-          <Route path='/aboutme' element={<AboutMe />} />
-          <Route path='/user' element={<User />} />
+          <Route element={<Layout />}>
+            <Route path='/' element={<Posts />} />
+            <Route path='/aboutme' element={<AboutMe />} />
+            <Route path='/user' element={<User />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </Provider>
