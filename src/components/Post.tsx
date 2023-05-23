b22937476ec362f8b9f771b3e10e607b9fec2337
AxiE1337 <pixell390@gmail.com>
2023-05-23 12:13:37 +0300

    added clickable property to post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -3,16 +3,23 @@ import { useNavigate } from 'react-router-dom'
 import Comment from './Comment'
 import styles from './index.module.scss'
 
-function Post({ title, body, postId, userId }: IPost) {
+function Post({ title, body, postId, userId, clickableAvatar = true }: IPost) {
   const navigate = useNavigate()
+
+  const handleClick = () => {
+    if (!clickableAvatar) return
+    navigate(`/user/${userId}`)
+  }
 
   return (
     <div className={styles.post}>
       <h3>{title}</h3>
       <p>{body}</p>
       <img
-        className={styles.postAvatar}
-        onClick={() => navigate(`/user/${userId}`)}
+        className={
+          clickableAvatar ? styles.postAvatar : styles.postAvatarNotClickable
+        }
+        onClick={handleClick}
         src='https://free.clipartof.com/855-Free-Clipart-Of-A-Male-Avatar.jpg'
         alt='avatar'
       />
@@ -28,4 +35,5 @@ interface IPost {
   body: string
   postId: number
   userId: number
+  clickableAvatar?: boolean
 }
--- src/pages/UserPage/User.tsx
+++ src/pages/UserPage/User.tsx
@@ -38,6 +38,7 @@ function User() {
           title={post.title}
           userId={post.userId}
           key={post.id}
+          clickableAvatar={false}
         />
       ))}
     </main>
