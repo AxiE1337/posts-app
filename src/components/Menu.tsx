0fb067e9925793b5b5fe13564af287a423dda163
AxiE1337 <pixell390@gmail.com>
2023-05-23 13:06:36 +0300

    changed burger menu btn made lines thiner

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,9 +11,9 @@ function Menu({ open, setClose }: IMenu) {
     <div className={styles.menu}>
       <Button onClick={handleOpen} variant='light' size='sm'>
         <svg viewBox='0 0 100 80' width='40' height='40'>
-          <rect width='100' height='20'></rect>
-          <rect y='30' width='100' height='20'></rect>
-          <rect y='60' width='100' height='20'></rect>
+          <rect width='100' height='10'></rect>
+          <rect y='30' width='100' height='10'></rect>
+          <rect y='60' width='100' height='10'></rect>
         </svg>
       </Button>
       {open && (
